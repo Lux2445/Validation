3d68f51efcfaa5acd6f8f8a719d92e566734af27
Lux2445 <yosinnurov2007@gmail.com>
2024-04-30 06:05:11 +0500

    adaptation

--- js/app.js
+++ js/app.js
@@ -1,118 +1,116 @@
-// let slideIndex = 1;
+const allDomains = ["google", "yandex", "mail", "gmail", "inbox"]
+const phoneNum = /^[\+]?[(]?[0-9]{2}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$/
+ // var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+function removeError(input) {
+    const parent = input.parentNode;
+    if (parent.classList.contains('error')) {
+        parent.querySelector('.error-label').remove()
+        parent.classList.remove('error')
+    }
+}
 
-// function currentSlide(n) {
-//     showSlide(slideIndex = n);
-// }
+function createError(input, text) {
+    const parent = input.parentNode;
+    const errorLabel = document.createElement('label')
+    errorLabel.classList.add('error-label')
+    errorLabel.textContent = text
+    parent.classList.add('error')
+    parent.append(errorLabel)
+}
 
-// function plusSlides(n) {
-//     showSlide(slideIndex += n);
-// }
-
-// function showSlide(n) {
-//     let i;
-//     const slides = document.querySelectorAll('.mySlides');
-//     const dots = document.querySelectorAll('.dot');
-    
-//     if (n > slides.length) {
-//         slideIndex = 1;
-//     }
-    
-//     if (n < 1) {
-//        slideIndex = slides.length;
-//     }
-    
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none"; // Скрыть все слайды
-//     }
-    
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].classList.remove("active"); // Убрать класс 'active' у всех точек
-//     }
-    
-//     slides[slideIndex - 1].style.display = "block"; // Показать текущий слайд
-//     dots[slideIndex - 1].classList.add("active"); // Добавить класс 'active' текущей точке
-// }
-
-// // Привязка обработчиков событий вне функции showSlide
-// document.querySelector('.prev').addEventListener('click', function() {
-//     plusSlides(-1);
-// });
-
-// document.querySelector('.next').addEventListener('click', function() {
-//     plusSlides(1);
-// });
-
-// const dots = document.querySelectorAll('.dot');
-// dots.forEach(function(dot, index) {
-//     dot.addEventListener('click', function() {
-//         currentSlide(index + 1);
-//     });
-// });
-
-// // Показать первый слайд при загрузке страницы
-// showSlide(slideIndex);
-const incorrectSybol = ["#", "*", "^", "$",]
 function validation(form) {
-    function removeError (input) {
-        const parent = input.parentNode
-        if(parent.classList.contains('error')){
-            parent.querySelector('.error-label').remove()
-            parent.classList.remove('error')
-        }
-    }
-    function createError(text,input) {
-        const parent = input.parentNode;
-        const errorLabel = document.createElement('label')
-        parent.classList.add('error')
-        errorLabel.classList.add('error-label')
-        errorLabel.textContent = text
-        parent.appendChild(errorLabel)
-    }
-    let result = true
-    const allInputs = form.querySelectorAll('input')
+    const allInputs = form.querySelectorAll('input');
+    let result = true;
+
+   
+
     for (const input of allInputs) {
-        removeError(input)
-        if(input.dataset.required == "true"){
-            if(input.value=="") {
-                console.log("Заполните форму")
-                createError("Поле не заполнено", input)
+        removeError(input)       
+
+        if (input.dataset.minLength) {
+            if (input.value.length < input.dataset.minLength) {
+                removeError(input)
+                createError(input, `Минимальное кол-во символов: ${input.dataset.minLength}`)
                 result = false
             }
         }
-        if(input.dataset.maxLeght){
-        if (input.value.lenght > input.dataset.maxLength){
-                console.log("Ошибка поля")
-                createError(`Максивмальное количество символов не должно превышать :${input.dataset.maxLeght}`, input)
+
+        if (input.dataset.maxLength) {
+            if (input.value.length > input.dataset.maxLength) {
+                removeError(input)
+                createError(input, `Максимальное кол-во символов: ${input.dataset.maxLength}`)
                 result = false
             }
         }
-        if(input.dataset.minLeght){
-            if (input.value.lenght < input.dataset.maxLength){
-                    console.log("Ошибка поля")
-                    createError(`минимальное количество символов не должно превышать :${input.dataset.minLeght}`, input)
+
+        if (input.dataset.required == "true") {
+            if (input.value == "") {
+                removeError(input)
+                createError(input, 'Поле не заполнено')
+                result = false
+            }
+        }
+
+        if(input.dataset.email) {
+            if(!input.value.includes('@')) {
+                removeError(input)
+                createError(input, `Введен некорректный адрес электронной почты`)                
+                result = false
+            } else {
+                const domain = input.value.split('@')[1].split('.')[0] //yandex,google
+                // console.log(domain)
+                if(!allDomains.includes(domain)) {
+                    createError(input, `Недопустимый домен почты: ${allDomains}`)                    
                     result = false
                 }
             }
-    form.querySelectorAll('input').forEach(element => {
-        console.log(element)
-    })
-    return result 
-    }}
+        }
 
-document.querySelector('#add-form').addEventListener('submit', function(event){
+        if(input.dataset.phone) {
+
+            if (input.value.match(phoneNum)) { 
+                removeError(input)             
+                
+            } else {
+                removeError(input)
+                createError(input, `Введен некорректный номер телефона`)               
+                result = false
+            }
+
+            // if(!input.value.includes('@')) {
+            //     removeError(input)
+            //     createError(input, `Введен некорректный адрес электронной почты`)                
+            //     result = false
+            // } else {
+            //     const domain = input.value.split('@')[1].split('.')[0] //yandex,google
+            //     // console.log(domain)
+            //     if(!allDomains.includes(domain)) {
+            //         createError(input, `Недопустимый домен почты: ${allDomains}`)                    
+            //         result = false
+            //     }
+            // }
+
+
+        }
+        // if (input.dataset.usermail) {
+        //     removeError(input)
+        //     if (input.value.match(mailformat)) {
+        //         createError(input, 'Valid email address!')           
+        //         return true;
+        //     } else {
+        //         createError(input, 'You have entered an invalid email address!') 
+                
+        //         return false;
+        //     }
+        // }
+
+    }
+    return result
+}
+
+document.querySelector('#add-form').addEventListener('submit', function (event){
     event.preventDefault()
-    if(validation(this)==true) {
+    if (validation(this) == true) {
         alert('Форма успешно заполнена!')
     }
-})
-
-document.querySelector('.link').addEventListener('click', function(event){
-    event.preventDefault()
-    alert('Вы нажали на ссылку, но перехода не произошло')
-})
-
-// const oninput = document.querySelector('.input_form')
-// oninput = function () {
-//     this.value = this.value.slice(0, 3)
-// }
-
+}) 
